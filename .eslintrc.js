cfeb549f2a3d8765d63fca82b59b588964d9f49c
Marina <lahniuk.m@gmail.com>
2020-12-05 19:48:52 +0300

    modify script eslint ./src, .eslintrc node true

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,8 @@
 module.exports = {
     "env": {
         "browser": true,
-        "es2021": true
+        "es2021": true,
+        "node": true
     },
     "root":true,
     "extends": "eslint:recommended",
