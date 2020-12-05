595548628646abc536f61e3e79076c849e0931cb
Marina <lahniuk.m@gmail.com>
2020-12-05 18:57:07 +0300

    final2. add eslint, husky

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "root":true,
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "ecmaVersion": 12,
+        "sourceType": "module"
+    },
+    "rules": {
+        "init-declarations": "off",
+        "no-console": "off",
+        "no-inline-comments": "off",  
+    }
+};
